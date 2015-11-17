e680c476559b3d825b0c9cc06afc58609f36bf6d
nathan wuenstel <njwuenstel@gmail.com>
2015-11-16 19:22:49 -0600

    recipient added

---
+++ src/enterprise/java/entity/Recipient.java
@@ -0,0 +1,29 @@
+package enterprise.java.entity;
+
+/**
+ * Created by Student on 11/16/2015.
+ */
+public class Recipient {
+
+    private String emailAddress;
+    private String recipientName;
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getRecipientName() {
+        return recipientName;
+    }
+
+    public void setRecipientName(String recipientName) {
+        this.recipientName = recipientName;
+    }
+
+
+
+}
